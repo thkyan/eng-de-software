80978628d51b4bac40284f1f4f20f6426261f7a0
Thayná de Matos Sousa <thaynamatos956@gmail.com>
2024-09-26 09:47:51 -0300

    class e metodos

--- 6.py
+++ 6.py
@@ -17,3 +17,23 @@ print(f"Idade: {pessoa1.idade}")
 
 pessoa1.aniversario()
 print(f"Idade nova: {pessoa1.idade}")
+
+
+
+class Animal:
+    def __init__(self, nome):
+        self.nome = nome
+    def fazer_barulho(self):
+        pass
+class Cachorro(Animal):
+    def fazer_barulho(self):
+        return "AuAu"
+class Gato(Animal):
+    def fazer_barulho(self):
+        return "Miau"
+    
+jully = Cachorro("jully")
+gata = Gato("gata")
+
+print(f"{jully.nome} faz: {jully.fazer_barulho()}")
+print(f"{gata.nome} faz: {gata.fazer_barulho()}")
