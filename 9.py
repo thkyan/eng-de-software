9e216457beb8afa1457525dbef05df4b78862c95
Thayná de Matos Sousa <thaynamatos956@gmail.com>
2024-10-01 14:46:41 -0300

    matplotlib

---
+++ 9.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+x = [1, 2, 3, 4, 5, 6]
+y = [2, 1, 4, 3, 6, 5]
+
+plt.plot(x, y)
+
+plt.xlabel("eixo x")
+plt.ylabel("eixo y")
+
+plt.title("exemplo de gráfico")
+
+plt.show()
